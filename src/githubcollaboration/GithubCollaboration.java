5c2cd69814e77ef09f6a411ea56828c47bacf17e
willc39 <williamcowden39@gmail.com>
2019-02-10 13:09:03 -0500

    Github Collaboration project commit

---
+++ src/githubcollaboration/GithubCollaboration.java
@@ -0,0 +1,19 @@
+package githubcollaboration;
+
+public class GithubCollaboration {
+
+    public static void main(String[] args) {
+        
+        Cowden();
+        
+    }
+    
+    public static void Cowden() {
+        
+        System.out.println("Farmer Will Cowden was here.");
+        
+    }
+    
+    //put your methods here and call then in the main
+    
+}
